615745155c9a05909bf6a54272c414616d69906b
8954sood <8954sood@naver.com>
2025-07-08 14:57:05 +0900

    feat: Create Boost Cog

---
+++ cogs/boost_cog.py
@@ -0,0 +1,20 @@
+import os
+
+import discord
+from discord.ext import commands
+from core.overwatch_bot import OverwatchBot
+
+class BoostCog(commands.Cog):
+    def __init__(self, bot: OverwatchBot):
+        self.bot = bot
+        self.boost_channel = int(os.getenv("BOOST_MESSAGE_SEND_CHANNEL"))
+
+    @commands.Cog.listener()
+    async def on_member_update(self, before: discord.Member, after: discord.Member):
+        if not before.premium_since and after.premium_since:
+            channel = self.bot.get_channel(self.boost_channel)  # 부스트 메시지를 보낼 채널 ID
+            if channel:
+                await channel.send(f"{after.mention}님, 서버를 부스트해주셔서 감사합니다! 🚀")
+
+async def setup(bot: OverwatchBot):
+    await bot.add_cog(BoostCog(bot))
